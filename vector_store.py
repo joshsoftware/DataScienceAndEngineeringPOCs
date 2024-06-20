7d3cde69c547c6b061bd0770f7212a936d2b925d
Prajjwalkumar <prajjwalkumar.panzade@joshsoftware.com>
2024-06-20 12:21:13 +0530

    LLM configuration

--- vector_store.py
+++ vector_store.py
@@ -1,12 +1,17 @@
 from qdrant_client import QdrantClient
-import os
-from dotenv import load_dotenv
+from llama_index.embeddings.fastembed import FastEmbedEmbedding
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import StorageContext, VectorStoreIndex, Settings
 
-load_dotenv()
-qdrant_api = os.getenv("Qdrant_API")
-qdrant_url = os.getenv("Qdrant_URL")
-
-client = QdrantClient(
-    url= qdrant_url,
-    api_key=qdrant_api,
-)
+class QdrantDataLoader:
+    def __init__(self, qdrant_api, qdrant_url):
+        self.client = QdrantClient(url=qdrant_url, api_key=qdrant_api)
+        self.index = None
+    
+    def load_data(self, documents):
+        embed_model = FastEmbedEmbedding(model_name="BAAI/bge-small-en-v1.5")
+        Settings.embed_model = embed_model
+        Settings.chunk_size = 512
+        vector_store = QdrantVectorStore(client=self.client, collection_name="test3")
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        self.index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
