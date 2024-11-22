34c1f4735f2e46a7e65179b90f1fe92690eedc06
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-11-22 11:45:05 +0530

    feat[vectors]: vec similarity search retrieval

--- db/schema.py
+++ db/schema.py
@@ -6,7 +6,9 @@
 # filePath -- Path to where data is stored
 
 from sqlmodel import SQLModel, Field
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, JSON
+from typing import Any
+from pgvector.sqlalchemy import Vector
 
 class ScrapData(SQLModel, table=True):
     id: int = Field(default=None, sa_column=Column(Integer, primary_key=True, autoincrement=True))
@@ -15,3 +17,7 @@ class ScrapData(SQLModel, table=True):
     websiteMaxNumberOfPages: int
     lastScrapedDate: str
     filePath: str
+class VectorData(SQLModel, table=True):
+    id: int = Field(default=None, sa_column=Column(Integer, primary_key=True, autoincrement=True))
+    metaData: dict = Field(sa_column=Column(JSON))
+    embedding: Any = Field(sa_column=Column(Vector(3)))
